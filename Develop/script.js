7c997cf7018c2f15e93fbade5a1ad47fe75c6852
clcoder2425 <141270027+clcoder2425@users.noreply.github.com>
2023-09-24 13:12:55 -0500

    added subtle code to js file

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+// 1. Prompt user for password criteria:
+//    a. Password length between 8 and 128 characters
+//    b. Password may include uppercase, lowerCase, numeric and special characters.
+// 2. Select password length
+// 3. Select which type of charactes to include
+// 4. Validate each input
+// 5. Display password
+function generatePassword() {
+  
+  var newpass ="test";
+  return newpass;
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
