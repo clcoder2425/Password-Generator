a1bf4ac9cddeea56b5378d3552c895873ea726a7
clcoder2425 <141270027+clcoder2425@users.noreply.github.com>
2023-09-24 19:59:39 -0500

    added comments to js file

--- Develop/script.js
+++ Develop/script.js
@@ -8,10 +8,12 @@ var generateBtn = document.querySelector("#generate");
 // 4. Validate each input
 // 5. Display password
 
+// define generatepassword function
 function generatePassword() {
+  //notify user of password criteria
 alert("your password must be between 8 and 128 characters");
 alert("uppercase, lowercase, numbers, special characters");
-
+//Confirm password length
 var passwordLength = prompt("Please enter your password length");
 
 if (passwordLength<8|| passwordLength>128){
@@ -21,6 +23,12 @@ if (passwordLength<8|| passwordLength>128){
 }else if (passwordLength>=8|| passwordLength<=128){
 console.log(passwordLength);
 alert("you have chosen a valid length");
+}
+//prompt user to choose password characters
+var upCase= prompt("would you like to include uppercase? ");
+if (upCase==="yes"){
+  var lowerCase= prompt("would you like to include lowercase");
+
 }
   var newpass ="test";
   return newpass;
