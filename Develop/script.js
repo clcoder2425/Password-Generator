0b5d5300059ceddfa86f31da00ea5729b3747b2e
clcoder2425 <141270027+clcoder2425@users.noreply.github.com>
2023-09-25 13:13:09 -0500

    variable declarations

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,12 @@
 // Assignment Code
+//Variables declaration
+var passwordLength= 8;
+var selectionarr =[];
+var upCasearr =['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var lowerCasearr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var specialChararr = ['@','#','$','%','&','!','(',')','?','*','^',];
+var numbersarr = ['0','1','2','3','4','5','6','7','8','0'];
+
 var generateBtn = document.querySelector("#generate");
 // 1. Prompt user for password criteria:
 //    a. Password length between 8 and 128 characters
@@ -11,10 +19,10 @@ var generateBtn = document.querySelector("#generate");
 // define generatepassword function
 function generatePassword() {
   //notify user of password criteria
-alert("your password must be between 8 and 128 characters");
-alert("uppercase, lowercase, numbers, special characters");
+alert("Password must be between 8 and 128 characters");
+alert("May contain uppercase, lowercase, numbers and special characters");
 //Confirm password length
-var passwordLength = prompt("Please enter your password length");
+var passwordLength = prompt("How many characters would you like for your password");
 
 if (passwordLength<8|| passwordLength>128){
   alert("password must be between 8 and 128 characters");
@@ -27,12 +35,22 @@ alert("you have chosen a valid length");
 //prompt user to choose password characters
 var upCase= prompt("would you like to include uppercase? ");
 if (upCase==="yes"){
+  upCase=[A,B,C,D,E,F,G,HIJKLMNOPQRSTUVWXYZ]
   var lowerCase= prompt("would you like to include lowercase");
+  if (lowerCase==="yes"){
+    var specialchar=prompt("Would like to include special characters?");
+    if(specialchar==="yes"){
+      var numbers=prompt("Would like to include numbers?");
+      if(numbers==="yes"){
 
+      }
+    }
+  }
 }
   var newpass ="test";
   return newpass;
 }
+
 
 // Write password to the #password input
 function writePassword() {
